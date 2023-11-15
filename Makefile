6b7f8fb5751547d4190aaf5eb61155ecfdf74c8f
Clecio Jung <clecio.jung@gmail.com>
2023-11-15 12:19:32 -0300

    (#2) Checking if the game is solvable Also refactored the code and simplified the Makefile Close #2

--- Makefile
+++ Makefile
@@ -4,53 +4,17 @@
 
 EXEC = 15puzzle
 
-# Folders
-ODIR = .obj
-DDIR = .deps
-SDIR = ./
-IDIR = ./
-
-# .c files
-SRCS = $(wildcard $(SDIR)/*.c $(SDIR)/*.cpp)
-
-# .h files
-INCS = $(wildcard $(IDIR)/*.h $(IDIR)/*.hpp)
-
-# Dependency files (auto generated)
-DEPS = $(patsubst %,%.d,$(basename $(subst $(SDIR),$(DDIR),$(SRCS))))
-
-# Object files
-OBJS = $(patsubst %,%.o,$(basename $(subst $(SDIR),$(ODIR),$(SRCS))))
-
 # ----------------------------------------
 # Compiler and linker definitions
 # ----------------------------------------
 
 # Compiler and linker
-#CC = gcc
-#CXX = g++
-
-# Libraries
-INCLUDES =
+ifndef CC
+	CC := gcc
+endif
 
 # Flags for compiler
-CFLAGS = -W -Wall -Wextra -pedantic -std=c99
-CXXFLAGS = -W -Wall -Wextra -pedantic
-DEPFLAGS = -MT $@ -MMD -MP -MF $(DDIR)/$*.Td
-
-# Compiler macros
-COMPILE.CC = $(CC) $(CFLAGS)
-COMPILE.CXX = $(CXX) $(CXXFLAGS)
-POSTCOMPILE = mv -f $(DDIR)/$*.Td $(DDIR)/$*.d && touch $@
-
-# ----------------------------------------
-# Fomating macros
-# ----------------------------------------
-
-BOLD = \033[1m
-NORMAL = \033[0m
-RED = \033[0;31m
-GREEN = \033[0;32m
+CFLAGS = -W -Wall -Wextra -pedantic -Wconversion -Wswitch-enum -Werror -flto -std=c99
 
 # ----------------------------------------
 # Compilation and linking rules
@@ -58,40 +22,15 @@ GREEN = \033[0;32m
 
 all: $(EXEC)
 
-$(EXEC): $(OBJS)
-	@ echo "${GREEN}Building binary: ${BOLD}$@${GREEN} using dependencies: ${BOLD}$^${NORMAL}"
-	$(COMPILE.CC) $(filter %.s %.o,$^) -o $@ $(INCLUDES)
-	@ touch $@
-
-$(ODIR)/%.o : $(SDIR)/%.c
-$(ODIR)/%.o : $(SDIR)/%.c $(DDIR)/%.d | $(DDIR) $(ODIR)
-	@ echo "${GREEN}Building target: ${BOLD}$@${GREEN}, using dependencies: ${BOLD}$^${NORMAL}"
-	$(COMPILE.CC) $(DEPFLAGS) -c $(filter %.c %.s %.o,$^) -o $@
-	@ $(POSTCOMPILE)
-
-$(ODIR)/%.o : $(SDIR)/%.cpp
-$(ODIR)/%.o : $(SDIR)/%.cpp $(DDIR)/%.d | $(DDIR) $(ODIR)
-		@ echo "${GREEN}Building target: ${BOLD}$@${GREEN}, using dependencies: ${BOLD}$^${NORMAL}"
-		$(COMPILE.CXX) $(DEPFLAGS) -c $(filter %.cpp %.s %.o,$^) -o $@
-		@ $(POSTCOMPILE)
-
-$(DDIR)/%.d: ;
-.PRECIOUS: $(DDIR)/%.d
-
--include $(DEPS)
+$(EXEC): main.c Makefile
+	$(CC) $(CFLAGS) $(filter %.c %.s %.o,$^) -o $@
 
 # ----------------------------------------
 # Script rules
 # ----------------------------------------
 
-$(DDIR):
-	mkdir -p $@
-
-$(ODIR):
-	mkdir -p $@
-
 clean:
-	rm -fr $(ODIR)/ $(DDIR)/ $(EXEC) $(SDIR)/*.gch *~ env.mk
+	git clean -fdx
 
 remade: clean all
 
--- main.c
+++ main.c
@@ -4,13 +4,13 @@
  *------------------------------------------------------------------------------
  */
 
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <time.h>
-#include <stdbool.h>
 #include <string.h>
 #include <termios.h>
+#include <time.h>
 #include <unistd.h>
 
 /*
@@ -22,6 +22,13 @@
 #define SIDES 4
 #define SQUARED_SIDES (SIDES * SIDES)
 #define PIECES (SIDES * SIDES - 1)
+#if PIECES < 10
+#define DIGITS 1
+#elif PIECES < 100
+#define DIGITS 2
+#else
+#error "Usnupported number of pieces!"
+#endif
 
 /*
  *------------------------------------------------------------------------------
@@ -39,59 +46,34 @@ unsigned int board[SQUARED_SIDES];
  *------------------------------------------------------------------------------
  */
 
-void printBoard(void)
-{
-    for (unsigned int i = 0; i < SQUARED_SIDES; i++)
-    {
-        if ((i % SIDES) == 0)
-        {
-            if (i != 0)
-            {
+void printBoard(void) {
+    for (unsigned int i = 0; i < SQUARED_SIDES; i++) {
+        if ((i % SIDES) == 0) {
+            if (i != 0) {
                 putchar('|');
             }
             printf("\n ");
-            for (unsigned int j = 0; j < SIDES; j++)
-            {
-                printf("----");
-#if (SIDES > 3)
-                putchar('-');
-#endif
+            for (unsigned int j = 0; j < SIDES; j++) {
+                printf("%.*s", DIGITS+3, "----------");
             }
             printf("-\n |");
-        }
-        else
-        {
+        } else {
             printf("|");
         }
-        if (board[i] == 0)
-        {
-            printf("   ");
-#if (SIDES > 3)
-            putchar(' ');
-#endif
-        }
-        else
-        {
-#if (SIDES < 4)
-            printf(" %d ", board[i]);
-#else
-            printf(" %02d ", board[i]);
-#endif
+        if (board[i] == 0) {
+            printf("%*s", DIGITS+2, "");
+        } else {
+            printf(" %.*d ", DIGITS, board[i]);
         }
     }
     printf("|\n ");
-    for (unsigned int j = 0; j < SIDES; j++)
-    {
-        printf("----");
-#if (SIDES > 3)
-        putchar('-');
-#endif
+    for (unsigned int j = 0; j < SIDES; j++) {
+        printf("%.*s", DIGITS+3, "----------");
     }
     printf("-\n ");
 }
 
-void rewindCursor(const unsigned int lines, const unsigned int columns)
-{
+void rewindCursor(const unsigned int lines, const unsigned int columns) {
     printf("\x1b[%dA", lines);
     printf("\x1b[%dD", columns);
 }
@@ -101,113 +83,120 @@ void rewindCursor(const unsigned int lines, const unsigned int columns)
 #define ARROW_RIGHT 'C'
 #define ARROW_LEFT 'D'
 
-char getArrowKey(void)
-{
-    while (getchar() != '\033')
-    {
+char getArrowKey(void) {
+    while (getchar() != '\033') {
     }
     getchar();
-    return getchar();
+    return (char)getchar();
 }
 
-void initBoardGame(void)
-{
-    bool usedNumbers[SQUARED_SIDES];
-    memset(usedNumbers, false, sizeof(usedNumbers));
-    srand((unsigned int)time(NULL));
-    for (unsigned int i = 0; i < SQUARED_SIDES; i++)
-    {
-        unsigned int randomNumber = rand() % SQUARED_SIDES;
-        if (!usedNumbers[randomNumber])
-        {
-            board[i] = randomNumber;
-            usedNumbers[randomNumber] = true;
-        }
-        else
-        {
-            i--;
-        }
-    }
-}
-
-unsigned int findEmptyPosition(void)
-{
-    for (unsigned int emptyPosition = 0; emptyPosition < SQUARED_SIDES; emptyPosition++)
-    {
-        if (!board[emptyPosition])
-        {
+unsigned int findEmptyPosition(void) {
+    for (unsigned int emptyPosition = 0; emptyPosition < SQUARED_SIDES; emptyPosition++) {
+        if (!board[emptyPosition]) {
             return emptyPosition;
         }
     }
     return 0;
 }
 
-void getPlayerMove(void)
-{
+// Count the number of permutations necessary to put the game board in the right order
+unsigned int permutations(void) {
+    unsigned int n = 0;
+    for (unsigned int i = 0; (i+1) < SQUARED_SIDES; i++) {
+        if (board[i] != 0) {
+            for (unsigned int j = (i+1); j < SQUARED_SIDES; j++) {
+                if ((board[j] != 0) && (board[i] > board[j])) {
+                    n++;
+                }
+            }
+        }
+    }
+    return n;
+}
+
+// https://mathworld.wolfram.com/15Puzzle.html
+// https://www.geeksforgeeks.org/check-instance-15-puzzle-solvable/
+bool isSolvable(void) {
+#if ((SIDES % 2) != 0) // odd
+    return ((permutations() % 2) == 0);
+#else // even
     const unsigned int emptyPosition = findEmptyPosition();
-    while (true)
-    {
-        switch (getArrowKey())
-        {
-        case ARROW_UP:
-            if (emptyPosition < (SQUARED_SIDES - SIDES))
-            {
-                board[emptyPosition] = board[(emptyPosition + SIDES)];
-                board[(emptyPosition + SIDES)] = 0;
-                return;
+    const unsigned int row = emptyPosition / SIDES + 1;
+    return ((row % 2) == 0) != ((permutations() % 2) != 0);
+#endif
+}
+
+void initBoardGame(void) {
+    bool usedNumbers[SQUARED_SIDES];
+    srand((unsigned int)time(NULL));
+    do {
+        memset(usedNumbers, false, sizeof(usedNumbers));
+        for (unsigned int i = 0; i < SQUARED_SIDES; i++) {
+            unsigned int randomNumber = (unsigned int)rand() % SQUARED_SIDES;
+            if (!usedNumbers[randomNumber]) {
+                board[i] = randomNumber;
+                usedNumbers[randomNumber] = true;
+            } else {
+                i--;
             }
-            break;
-        case ARROW_DOWM:
-            if (emptyPosition >= SIDES)
-            {
-                board[emptyPosition] = board[(emptyPosition - SIDES)];
-                board[(emptyPosition - SIDES)] = 0;
-                return;
-            }
-            break;
-        case ARROW_RIGHT:
-            if ((emptyPosition % SIDES) != 0)
-            {
-                board[emptyPosition] = board[(emptyPosition - 1)];
-                board[(emptyPosition - 1)] = 0;
-                return;
-            }
-            break;
-        case ARROW_LEFT:
-            if ((emptyPosition % SIDES) != (SIDES - 1))
-            {
-                board[emptyPosition] = board[(emptyPosition + 1)];
-                board[(emptyPosition + 1)] = 0;
-                return;
-            }
-            break;
+        }
+    } while (!isSolvable());
+}
+
+void getPlayerMove(void) {
+    const unsigned int emptyPosition = findEmptyPosition();
+    while (true) {
+        switch (getArrowKey()) {
+            case ARROW_UP:
+                if (emptyPosition < (SQUARED_SIDES - SIDES)) {
+                    board[emptyPosition] = board[(emptyPosition + SIDES)];
+                    board[(emptyPosition + SIDES)] = 0;
+                    return;
+                }
+                break;
+            case ARROW_DOWM:
+                if (emptyPosition >= SIDES) {
+                    board[emptyPosition] = board[(emptyPosition - SIDES)];
+                    board[(emptyPosition - SIDES)] = 0;
+                    return;
+                }
+                break;
+            case ARROW_RIGHT:
+                if ((emptyPosition % SIDES) != 0) {
+                    board[emptyPosition] = board[(emptyPosition - 1)];
+                    board[(emptyPosition - 1)] = 0;
+                    return;
+                }
+                break;
+            case ARROW_LEFT:
+                if ((emptyPosition % SIDES) != (SIDES - 1)) {
+                    board[emptyPosition] = board[(emptyPosition + 1)];
+                    board[(emptyPosition + 1)] = 0;
+                    return;
+                }
+                break;
         }
     }
 }
 
-bool continueGame(void)
-{
-    for (unsigned int i = 0; i < (SQUARED_SIDES - 1); i++)
-    {
-        if (board[i] != (i + 1))
-        {
+bool continueGame(void) {
+    for (unsigned int i = 0; i < (SQUARED_SIDES - 1); i++) {
+        if (board[i] != (i + 1)) {
             return true;
         }
     }
     return false;
 }
 
-void restoreTerminal(void)
-{
+void restoreTerminal(void) {
     tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
 }
 
-void configureTerminal(void)
-{
+void configureTerminal(void) {
     tcgetattr(STDIN_FILENO, &oldt);
     newt = oldt;
-    newt.c_lflag &= ~ICANON;
-    newt.c_lflag &= ~ECHO;
+    newt.c_lflag &= (unsigned int)~ICANON;
+    newt.c_lflag &= (unsigned int)~ECHO;
     tcsetattr(STDIN_FILENO, TCSANOW, &newt);
     atexit(restoreTerminal);
 }
@@ -218,19 +207,18 @@ void configureTerminal(void)
  *------------------------------------------------------------------------------
  */
 
-int main(void)
-{
+int main(void) {
     printf("Instructions: Use keyboard arrow keys to move the pieces!\n");
     configureTerminal();
     initBoardGame();
+    printf("Number of permutations: %u\n", permutations());
     printBoard();
-    while (continueGame())
-    {
+    while (continueGame()) {
         getPlayerMove();
         rewindCursor(2 * (SIDES + 1), 0);
         printBoard();
     }
-    printf("\nCongratulations! You ended the game!\n");
+    printf("\nCongratulations! You solved the puzzle!\n");
     return EXIT_SUCCESS;
 }
 
